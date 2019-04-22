e26d00fb0a8d5a72fdddea121cc1fb43be1b6ffe
Ekaterina Kadyrova <pin-ka@outlook.com>
2019-04-23 00:58:22 +0500

    Для исследования жизненного цикла добавлена 2ая активность, созданы тосты и информационные логи для отслеживания жизненного цикла 1ой активности, в первую активность выведены погодные значения

--- app/src/main/java/com/pinka/weather1/MainActivity.java
+++ app/src/main/java/com/pinka/weather1/MainActivity.java
@@ -1,13 +1,63 @@
 package com.pinka.weather1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "LifeCycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate");
+        Toast.makeText(getApplicationContext(),"onCreate",Toast.LENGTH_SHORT).show();
         setContentView(R.layout.activity_main);
+        findViewById(R.id.toSecondActivity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+        Toast.makeText(getApplicationContext(),"onStart",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+        Toast.makeText(getApplicationContext(),"onResume",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+        Toast.makeText(getApplicationContext(),"onPause",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+        Toast.makeText(getApplicationContext(),"onStop",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+        Toast.makeText(getApplicationContext(),"onRestart",Toast.LENGTH_SHORT).show();
     }
 }
