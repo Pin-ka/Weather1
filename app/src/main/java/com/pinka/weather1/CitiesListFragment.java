9e4d5ac95ae556952db880b3492293701bccb6e2
Ekaterina Kadyrova <pin-ka@outlook.com>
2019-05-21 03:48:11 +0500

    Список городов реализован с посощью CardView. Детальная информация о погоде реализована с помощью CoordinatorLayout.

--- app/src/main/java/com/pinka/weather1/CitiesListFragment.java
+++ app/src/main/java/com/pinka/weather1/CitiesListFragment.java
@@ -1,5 +1,6 @@
 package com.pinka.weather1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -11,17 +12,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.Objects;
 
 
 public class CitiesListFragment extends Fragment {
     private ListView listView;
-    private TextView emptyTextView;
 
     int currentPosition = 0;
 
@@ -37,7 +35,7 @@ public class CitiesListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         initViews(view);
-        initList();
+        initList(getContext());
     }
 
     @Override
@@ -66,7 +64,6 @@ public class CitiesListFragment extends Fragment {
 
     private void initViews(View view) {
         listView = view.findViewById(R.id.cities_list_view);
-        emptyTextView=view.findViewById(R.id.cities_list_empty_view);
         ImageView settingsBtn = view.findViewById(R.id.btnSettings);
         settingsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,11 +75,16 @@ public class CitiesListFragment extends Fragment {
         });
     }
 
-    private void initList() {
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(Objects.requireNonNull(getActivity()),
-                R.array.Cities, android.R.layout.simple_list_item_activated_1);
+    private void initList(Context context) {
+        CityData[]data=new CityData[]{
+             new CityData(getResources().getStringArray(R.array.Cities)[0]),
+                new CityData(getResources().getStringArray(R.array.Cities)[1]),
+                new CityData(getResources().getStringArray(R.array.Cities)[2]),
+                new CityData(getResources().getStringArray(R.array.Cities)[3]),
+                new CityData(getResources().getStringArray(R.array.Cities)[4])
+        };
+        CityListAdapter adapter=new CityListAdapter(context,data);
         listView.setAdapter(adapter);
-        listView.setEmptyView(emptyTextView);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
